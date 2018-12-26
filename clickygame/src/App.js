5aabc1120134a45890dbecb8c4b9f8dbd2a46ae1
dulcineapena1 <f.dulcinea.pena@gmail.com>
2018-12-26 15:03:34 -0600

    all functionality

--- clickygame/src/App.js
+++ clickygame/src/App.js
@@ -5,49 +5,45 @@ import Title from "./components/Title";
 import friends from "./friends.json";
 import "./App.css";
 
+
 class App extends Component {
   // friens trae los elementos del archivo json (aquí arriba está en import)
   state = {
     friends,
     friendsALosQueLeDiClick: [0],
     scoregeneral:0,
-    scoreMasAlto:0
+    scoreMasAlto:0,
+    mensaje:"",
+    cambio: { background: "blue", opacity:0.5} // son los estilos css que estaré cambiando según acierte o no
   };
 
   start = id => {
-   
+    // Esto es para sacar el top score (scoreMasAlto) Enrealidad estoy metiendo a una variable normal un valor de un state
     let nuevoScoreAlto= this.state.scoreMasAlto;
     this.state.scoregeneral >= nuevoScoreAlto ? nuevoScoreAlto= this.state.scoreMasAlto+1 : nuevoScoreAlto=nuevoScoreAlto
 
+    //Aquí almaceno los id a los que se le dio click
     this.setState({ friendsALosQueLeDiClick: this.state.friendsALosQueLeDiClick + id });
     
+    // Si el id clickeado está en la variable que almacena los clickados, entonces setea scores
     const friendly= this.state.friendsALosQueLeDiClick.includes(id);
-   // friendly ? this.setState({scoregeneral : 0, friendsALosQueLeDiClick:[0], scoreMasAlto:this.state.scoreMasAlto})  : this.setState ({scoregeneral : this.state.scoregeneral +1, scoreMasAlto: this.scoregeneral})
-    friendly ? this.setState({scoregeneral : 0, friendsALosQueLeDiClick:[0], scoreMasAlto:this.state.scoreMasAlto})  : this.setState((state) => { return {scoregeneral : state.scoregeneral +1, scoreMasAlto: nuevoScoreAlto}  })
-
-    //  this.saberElScoreMasAlto();
-  
-    // Pasando un state como prop, en el return primero solo dice el nombre del state envez de this.state.scoreMasAlto, porque ya se lo estás pasando como prop
-    //this.state.scoregeneral > this.state.scoreMasAlto  ? this.setState((state) => { return {scoreMasAlto: state.scoregeneral}  }) : this.setState({scoreMasAlto: this.state.scoreMasAlto})
-
-    
+    friendly ? this.setState({cambio:{background:"red",opacity:0.5}, mensaje:"Wrong! Try Again", scoregeneral : 0, friendsALosQueLeDiClick:[0], scoreMasAlto:this.state.scoreMasAlto})  : this.setState((state) => { return {mensaje: "", scoregeneral : state.scoregeneral +1, scoreMasAlto: nuevoScoreAlto, cambio:{background:"blue",opacity:1}}  })
+    // Pasando un state como prop, en el return primero solo dice el nombre del state envez de this.state.scoreMasAlto, porque ya se lo estás pasando como prop    
+   
   };
   
-  // saberElScoreMasAlto = () => {
-  //   let nuevo= this.state.scoregeneral 
-  //   let nuevo2= this.state.scoreMasAlto +1
-  //   nuevo >=  nuevo2  ? this.setState({scoreMasAlto: this.state.scoreMasAlto+1}  ) : this.setState({scoreMasAlto: this.state.scoreMasAlto})
-  // }
+ 
  
   
 
   render() {
     return (
-      <Wrapper>
+      <Wrapper style = {this.state.cambio}>
         <Title
           score= {this.state.scoregeneral}
           topscore= {this.state.scoreMasAlto}
-          diclick= {this.state.friendsALosQueLeDiClick}
+          mensajeNoAcerto= {this.state.mensaje}
+         // diclick= {this.state.friendsALosQueLeDiClick}    Con esto puedo saber a que id le estoy dando click
         />
 
         {/* // Primero mapeamos el json de friend y luego le damos el valor mathrandom al friend, y luego hacemos que acomode los elementos y al final mapeamos metiendo el efecto random */}
@@ -63,12 +59,7 @@ class App extends Component {
                 />)
               )
             )
-        }
-
-      
-
-       
-  
+        }  
       </Wrapper>
     );
   }
--- clickygame/src/components/Title/Title.js
+++ clickygame/src/components/Title/Title.js
@@ -2,11 +2,13 @@ import React from "react";
 import "./Title.css";
 
 const Title = props => 
-    <nav className="navbar navbar-expand-lg navbar-light bg-light">
-        Clicky game
+    <nav className="navbar navbar-expand-lg navbar-light bg-blue">
+        
+        <p>{props.mensajeNoAcerto}</p>
         <p>Score: {props.score}</p>
         <p>Top Score: {props.topscore}</p>
-        <p>relleno: {props.diclick}</p>
+        
+        {/* <p>relleno: {props.diclick}</p>    Con esto puedo saber a que id le doy click en app.js*/}
         
     </nav>;
 
